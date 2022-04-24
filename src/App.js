4cbc075bf56b34d44335fbe47f80b7d282cbc413
moohabir <87937848+moohabir@users.noreply.github.com>
2022-04-24 14:41:28 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [product, setProduct] = useState("products");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/${product}`)
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, [product]);
+
+  useEffect(() => {
+    console.log("useEffects created");
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
+
+  const handle = () => {
+    //above is an Arrow function
+    setWindowWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handle);
+    // Below is a useEffect clean up function
+    return () => {
+      window.removeEventListener("resize", handle);
+    };
+  }, [windowWidth]);
+
+  return (
+    <>
+      <div className="App">
+        <h1>Blog</h1>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+        <h2>{resourceType}</h2>
+        {items.map((item) => {
+          return <pre key={item.id}>{JSON.stringify(item)} </pre>;
+        })}
+        <h1>{windowWidth}</h1>
+      </div>
+
+      <div className="App">
+        <h1>FAKE Store API</h1>
+        <button onClick={() => setProduct("products")}>Products</button>
+        <button onClick={() => setProduct("carts")}>Cart</button>
+        <button onClick={() => setProduct("users")}>Users</button>
+        <button onClick={() => setProduct("login")}>Login</button>
+        <h2>{product}</h2>
+        {products.map((product) => {
+          return <pre key={product.id}>{JSON.stringify(product)} </pre>;
+        })}
+      </div>
+    </>
+  );
+}
